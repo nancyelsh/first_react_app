ca5182693b3d74256da251add92e8157b845b096
Nour El-Sharkawi <Nour@Nours-MacBook-Pro-703.local>
2019-11-17 19:32:04 +0300

    joinocded

---
+++ App.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import { Image } from "react-native";
+import {
+  Container,
+  CardItem,
+  Thumbnail,
+  Card,
+  Footer,
+  FooterTab,
+  Header,
+  Left,
+  Body,
+  Right,
+  Button,
+  Icon,
+  Title,
+  Text
+} from "native-base";
+
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name="arrow-back" />
+              <Text>Back</Text>
+            </Button>
+          </Left>
+          <Body>
+            <Title>Hey, hey!</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Text>Cancel</Text>
+            </Button>
+          </Right>
+        </Header>
+        <Card>
+          <CardItem>
+            <Left>
+              <Icon active name="heart" />
+              {/* <Thumbnail source={{ uri: "Image URL" }} /> */}
+              <Body>
+                <Text>First Instagram Memory</Text>
+                <Text note>GeekyNoobs</Text>
+              </Body>
+            </Left>
+          </CardItem>
+          <CardItem cardBody>
+            <Image
+              source={{
+                uri:
+                  "http://goodbettrbest.com/wp-content/uploads/2018/05/Barmej-Team-1440x1080.jpeg"
+              }}
+              style={{ height: 400, width: 585, flex: 1, borderRadius: 20 }}
+            />
+          </CardItem>
+          <CardItem>
+            <Left>
+              <Button transparent>
+                <Icon active name="thumbs-up" />
+                <Text>12 Likes</Text>
+              </Button>
+            </Left>
+            <Body>
+              <Button transparent>
+                <Icon active name="chatbubbles" />
+                <Text>4 Comments</Text>
+              </Button>
+            </Body>
+            <Right>
+              <Text>11h ago</Text>
+            </Right>
+          </CardItem>
+        </Card>
+        <Footer style={{ bottom: 0, position: "absolute" }}>
+          <FooterTab>
+            <Button vertical>
+              <Icon name="apps" />
+              <Text>Apps</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="camera" />
+              <Text>Camera</Text>
+            </Button>
+            <Button vertical active>
+              <Icon active name="navigate" />
+              <Text>Navigate</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="person" />
+              <Text>Contact</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
